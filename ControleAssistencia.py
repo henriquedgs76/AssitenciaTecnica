5c10647628845966d8f68cc224995b07d2ac3399
henriquedgs76 <douglasemanu2017@gmail.com>
2023-12-18 10:51:32 -0300

    Adiçao de novas funcionalidades

--- ControleAssistencia.py
+++ ControleAssistencia.py
@@ -1,6 +1,8 @@
 import sqlite3
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox, QTableWidget, QTableWidgetItem, QHBoxLayout, QHeaderView
-from PyQt5.QtCore import Qt, QDate, QTime, QDateTime
+from PyQt5.QtCore import Qt, QDate, QDateTime
+from PyQt5.QtGui import QFont
+import sys
 
 class ControleAssistencia:
     def __init__(self, nome_banco='controle_assistencia.db'):
@@ -11,7 +13,7 @@ class ControleAssistencia:
     def _conectar(self):
         try:
             self.conn = sqlite3.connect(self.nome_banco)
-            self.conn.execute('PRAGMA foreign_keys = ON')  
+            self.conn.execute('PRAGMA foreign_keys = ON')
             self.cursor = self.conn.cursor()
         except sqlite3.Error as e:
             print(f"Erro ao conectar ao banco de dados: {e}")
@@ -37,6 +39,8 @@ class ControleAssistencia:
             CREATE TABLE IF NOT EXISTS estoque (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 produto TEXT NOT NULL,
+                marca TEXT NOT NULL,
+                modelo TEXT NOT NULL,
                 quantidade INTEGER NOT NULL
             )
         ''')
@@ -45,7 +49,8 @@ class ControleAssistencia:
             CREATE TABLE IF NOT EXISTS ordens_servico (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 cliente TEXT NOT NULL,
-                modelo_computador TEXT,
+                marca TEXT NOT NULL,
+                modelo_computador TEXT NOT NULL,
                 descricao_problema TEXT,
                 data_entrada DATETIME DEFAULT CURRENT_TIMESTAMP,
                 data_conclusao DATETIME,
@@ -53,9 +58,16 @@ class ControleAssistencia:
             )
         ''')
 
-    def adicionar_produto(self, produto, quantidade):
+    def adicionar_produto(self, produto, marca, modelo, quantidade):
         try:
-            self._executar_query('INSERT INTO estoque (produto, quantidade) VALUES (?, ?)', (produto, quantidade))
+            quantidade = int(quantidade)
+        except ValueError:
+            print('A quantidade deve ser um número inteiro.')
+            return
+
+        try:
+            self._executar_query('INSERT INTO estoque (produto, marca, modelo, quantidade) VALUES (?, ?, ?, ?)', (produto, marca, modelo, quantidade))
+            print(f"Produto '{produto}' adicionado ao estoque.")
         except sqlite3.Error as e:
             print(f"Erro ao adicionar produto: {e}")
 
@@ -67,12 +79,13 @@ class ControleAssistencia:
             print(f"Erro ao listar estoque: {e}")
             return []
 
-    def criar_ordem_servico(self, cliente, modelo_computador, descricao_problema):
+    def criar_ordem_servico(self, cliente, marca, modelo_computador, descricao_problema):
         try:
             self._executar_query('''
-                INSERT INTO ordens_servico (cliente, modelo_computador, descricao_problema)
-                VALUES (?, ?, ?)
-            ''', (cliente, modelo_computador, descricao_problema))
+                INSERT INTO ordens_servico (cliente, marca, modelo_computador, descricao_problema)
+                VALUES (?, ?, ?, ?)
+            ''', (cliente, marca, modelo_computador, descricao_problema))
+            print(f"Ordem de serviço para '{cliente}' criada.")
         except sqlite3.Error as e:
             print(f"Erro ao criar ordem de serviço: {e}")
 
@@ -86,10 +99,8 @@ class ControleAssistencia:
 
     def marcar_ordem_concluida(self, id_ordem):
         try:
-            
             data_atual = QDate.currentDate()
-            data_conclusao = QDateTime(data_atual, QTime(0, 0, 0))
-
+            data_conclusao = QDateTime(data_atual)
             self._executar_query('UPDATE ordens_servico SET status = "Concluída", data_conclusao = ? WHERE id = ?', (data_conclusao.toString(Qt.ISODate), id_ordem))
         except sqlite3.Error as e:
             print(f"Erro ao marcar ordem como concluída: {e}")
@@ -104,82 +115,81 @@ class ControleAssistenciaGUI(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle('Assistência Técnica Henrique')
+        self.setGeometry(100, 100, 800, 600)
+
         self.controle_assistencia = ControleAssistencia()
 
         self.init_ui()
 
     def init_ui(self):
-        
         titulo_label = QLabel('Assistência Técnica Henrique', self)
         titulo_label.setAlignment(Qt.AlignCenter)
+        titulo_font = QFont("Arial", 16, QFont.Bold)
+        titulo_label.setFont(titulo_font)
 
-        label_produto = QLabel('Produto:')
-        self.entry_produto = QLineEdit()
+        self.entry_cliente = QLineEdit(self)
+        self.entry_cliente.setPlaceholderText('Cliente')
 
-        label_quantidade = QLabel('Quantidade:')
-        self.entry_quantidade = QLineEdit()
+        self.entry_marca = QLineEdit(self)
+        self.entry_marca.setPlaceholderText('Marca')
+
+        self.entry_modelo = QLineEdit(self)
+        self.entry_modelo.setPlaceholderText('Modelo do Computador')
+
+        self.entry_descricao = QLineEdit(self)
+        self.entry_descricao.setPlaceholderText('Descrição do Problema')
+
+        self.entry_quantidade = QLineEdit(self)
+        self.entry_quantidade.setPlaceholderText('Quantidade')
+
+        botoes_layout = QHBoxLayout()
+        botoes_layout.setAlignment(Qt.AlignCenter)
 
         btn_adicionar_produto = QPushButton('Adicionar Produto')
         btn_adicionar_produto.clicked.connect(self.adicionar_produto)
 
-        label_listar_estoque = QLabel('Listar Estoque:')
         btn_listar_estoque = QPushButton('Listar Estoque')
         btn_listar_estoque.clicked.connect(self.listar_estoque)
-
-        label_cliente = QLabel('Cliente:')
-        self.entry_cliente = QLineEdit()
-
-        label_modelo = QLabel('Modelo:')
-        self.entry_modelo = QLineEdit()
-
-        label_descricao = QLabel('Descrição:')
-        self.entry_descricao = QLineEdit()
 
         btn_criar_ordem = QPushButton('Criar Ordem de Serviço')
         btn_criar_ordem.clicked.connect(self.criar_ordem_servico)
 
-        label_listar_ordens = QLabel('Listar Ordens de Serviço:')
         btn_listar_ordens = QPushButton('Listar Ordens de Serviço')
         btn_listar_ordens.clicked.connect(self.listar_ordens_servico)
 
+        botoes_layout.addWidget(btn_adicionar_produto)
+        botoes_layout.addWidget(btn_listar_estoque)
+        botoes_layout.addWidget(btn_criar_ordem)
+        botoes_layout.addWidget(btn_listar_ordens)
+
         self.table_ordens_servico = QTableWidget(self)
-        self.table_ordens_servico.setColumnCount(9)  
-        self.table_ordens_servico.setHorizontalHeaderLabels(['ID', 'Cliente', 'Modelo', 'Descrição', 'Data Entrada', 'Data Conclusão', 'Status', 'Concluir', 'Excluir'])
+        self.table_ordens_servico.setColumnCount(9)
+        self.table_ordens_servico.setHorizontalHeaderLabels(['ID', 'Cliente', 'Marca', 'Modelo', 'Descrição', 'Data Entrada', 'Data Conclusão', 'Status', 'Excluir'])
         self.table_ordens_servico.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-        layout = QVBoxLayout()
-        layout.addWidget(titulo_label)  
-        layout.addWidget(label_produto)
-        layout.addWidget(self.entry_produto)
-        layout.addWidget(label_quantidade)
-        layout.addWidget(self.entry_quantidade)
-        layout.addWidget(btn_adicionar_produto)
+        layout_principal = QVBoxLayout()
+        layout_principal.addWidget(titulo_label)
+        layout_principal.addWidget(self.entry_cliente)
+        layout_principal.addWidget(self.entry_marca)
+        layout_principal.addWidget(self.entry_modelo)
+        layout_principal.addWidget(self.entry_descricao)
+        layout_principal.addWidget(self.entry_quantidade)
+        layout_principal.addLayout(botoes_layout)
+        layout_principal.addWidget(self.table_ordens_servico)
 
-        layout.addWidget(label_listar_estoque)
-        layout.addWidget(btn_listar_estoque)
-
-        layout.addWidget(label_cliente)
-        layout.addWidget(self.entry_cliente)
-        layout.addWidget(label_modelo)
-        layout.addWidget(self.entry_modelo)
-        layout.addWidget(label_descricao)
-        layout.addWidget(self.entry_descricao)
-        layout.addWidget(btn_criar_ordem)
-
-        layout.addWidget(label_listar_ordens)
-        layout.addWidget(btn_listar_ordens)
-        layout.addWidget(self.table_ordens_servico)
-
-        self.setLayout(layout)
+        self.setLayout(layout_principal)
 
         self.listar_ordens_servico()
 
     def adicionar_produto(self):
-        produto = self.entry_produto.text()
+        produto = self.entry_cliente.text()
+        marca = self.entry_marca.text()
+        modelo = self.entry_modelo.text()
         quantidade = self.entry_quantidade.text()
 
-        if produto and quantidade:
-            self.controle_assistencia.adicionar_produto(produto, int(quantidade))
+        if produto and marca and modelo and quantidade:
+            self.controle_assistencia.adicionar_produto(produto, marca, modelo, int(quantidade))
             QMessageBox.information(self, 'Sucesso', f"Produto '{produto}' adicionado ao estoque.")
         else:
             QMessageBox.critical(self, 'Erro', 'Por favor, preencha todos os campos.')
@@ -194,11 +204,12 @@ class ControleAssistenciaGUI(QWidget):
 
     def criar_ordem_servico(self):
         cliente = self.entry_cliente.text()
+        marca = self.entry_marca.text()
         modelo = self.entry_modelo.text()
         descricao = self.entry_descricao.text()
 
-        if cliente and modelo and descricao:
-            self.controle_assistencia.criar_ordem_servico(cliente, modelo, descricao)
+        if cliente and marca and modelo and descricao:
+            self.controle_assistencia.criar_ordem_servico(cliente, marca, modelo, descricao)
             QMessageBox.information(self, 'Sucesso', f"Ordem de serviço para '{cliente}' criada.")
             self.listar_ordens_servico()
         else:
@@ -213,25 +224,9 @@ class ControleAssistenciaGUI(QWidget):
             for col, value in enumerate(ordem):
                 self.table_ordens_servico.setItem(row, col, QTableWidgetItem(str(value)))
 
-            
-            btn_concluir = QPushButton('Concluir')
-            btn_concluir.clicked.connect(lambda _, row=row: self.marcar_concluido(row))
-            self.table_ordens_servico.setCellWidget(row, len(ordem), btn_concluir)
-
-            
             btn_excluir = QPushButton('Excluir')
             btn_excluir.clicked.connect(lambda _, row=row: self.excluir_ordem_servico(row))
-            self.table_ordens_servico.setCellWidget(row, len(ordem) + 1, btn_excluir)
-
-    def marcar_concluido(self, row):
-        id_ordem = self.table_ordens_servico.item(row, 0).text()
-        confirmacao = QMessageBox.question(self, 'Concluir Ordem de Serviço', f'Deseja marcar a Ordem de Serviço {id_ordem} como concluída?',
-                                           QMessageBox.Yes | QMessageBox.No)
-
-        if confirmacao == QMessageBox.Yes:
-            self.controle_assistencia.marcar_ordem_concluida(int(id_ordem))
-            QMessageBox.information(self, 'Sucesso', f'Ordem de Serviço {id_ordem} concluída com sucesso.')
-            self.listar_ordens_servico()
+            self.table_ordens_servico.setCellWidget(row, len(ordem), btn_excluir)
 
     def excluir_ordem_servico(self, row):
         id_ordem = self.table_ordens_servico.item(row, 0).text()
@@ -244,7 +239,7 @@ class ControleAssistenciaGUI(QWidget):
             self.listar_ordens_servico()
 
 if __name__ == '__main__':
-    app = QApplication([])
+    app = QApplication(sys.argv)
     ex = ControleAssistenciaGUI()
     ex.show()
-    app.exec_()
+    sys.exit(app.exec_())
